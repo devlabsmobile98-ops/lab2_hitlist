ce30d17c3363979da5a5fe22c5ffd4e872709b38
Nuha Haq <nuhahaq787@gmail.com>
2025-09-19 14:05:29 -0400

    All changes so far - download necessary attachments.

---
+++ app/src/main/java/com/example/hitlist/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hitlist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val imageButton5: ImageButton = findViewById(R.id.imageButton5)
+
+        imageButton5.setOnClickListener {
+            val intent = Intent(this, Homepage::class.java)
+            startActivity(intent)
+        }
+    }
+}
