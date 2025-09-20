8cc0303fea28db6ae5b2100511c8c538db214986
suwangying <suwangying619@gmail.com>
2025-09-19 22:55:14 -0400

    newTask page

--- app/src/main/java/com/example/hitlist/Homepage.kt
+++ app/src/main/java/com/example/hitlist/Homepage.kt
@@ -13,7 +13,7 @@ class Homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.display_date)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -32,7 +32,7 @@ class Homepage : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, NewTask::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.enter_left, R.anim.leave_right)
         }
--- app/src/main/java/com/example/hitlist/NewTask.kt
+++ app/src/main/java/com/example/hitlist/NewTask.kt
@@ -7,13 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.Calendar
+import android.app.DatePickerDialog
+import android.widget.ImageView
+import android.widget.TextView
 
 class NewTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_new_task)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.display_date)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -29,4 +33,5 @@ class NewTask : AppCompatActivity() {
         }
 
     }
-}
+        }
+
