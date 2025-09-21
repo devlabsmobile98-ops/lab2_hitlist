18d78b6b27cf978aeb8e3e6234a0646f5c1fb09e
suwangying <suwangying619@gmail.com>
2025-09-20 20:58:19 -0400

    newTask page

--- app/src/main/java/com/example/hitlist/NewTask.kt
+++ app/src/main/java/com/example/hitlist/NewTask.kt
@@ -9,8 +9,12 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.Calendar
 import android.app.DatePickerDialog
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class NewTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +29,11 @@ class NewTask : AppCompatActivity() {
 
         // Back Button to Homepage
         val fab3: FloatingActionButton = findViewById(R.id.fab3)
+        val calendarIcon: ImageView=findViewById(R.id.calendarIcon)
+        val buttonNext: Button=findViewById(R.id.buttonNext)
+        val dateShow: TextView=findViewById(R.id.dateShow)
+        val input_task: EditText=findViewById(R.id.input_task)
+        val description_box: EditText=findViewById(R.id.description_box)
 
         fab3.setOnClickListener {
             val intent = Intent(this, Homepage::class.java)
@@ -32,6 +41,38 @@ class NewTask : AppCompatActivity() {
             overridePendingTransition(R.anim.enter_left, R.anim.leave_right)
         }
 
-    }
+        buttonNext.setOnClickListener {
+            val task = input_task.text.toString()
+
+            if (task.isNotEmpty()) {
+                description_box.visibility = View.VISIBLE
+                description_box.requestFocus()
+                calendarIcon.visibility= View.VISIBLE
+            } else {
+                Toast.makeText(this, "Please enter a task first", Toast.LENGTH_SHORT).show()
+            }
+            calendarIcon.setOnClickListener {
+                val cal = Calendar.getInstance()
+                val year = cal.get(Calendar.YEAR)
+                val month = cal.get(Calendar.MONTH)
+                val day = cal.get(Calendar.DAY_OF_MONTH)
+
+
+                val datePicker = DatePickerDialog(
+                    this,
+                    { _, selectedYear, selectedMonth, selectedDay ->
+                        val date = "Date selected: $selectedDay/${selectedMonth + 1}/$selectedYear"
+                        dateShow.text = date
+                        dateShow.visibility = View.VISIBLE
+                    },
+                    year,
+                    month,
+                    day
+                )
+                datePicker.show()
+
         }
+
+    }
+        }}
 
